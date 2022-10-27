332ad5aa91b3381761a3fb9ff4536ed51e81c88a
Awnrt <74102659+Awnrt@users.noreply.github.com>
2022-10-27 16:59:27 +0300

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "undefined-medium:pixelsize=24:antialias=true:autohint=true:";
+static char *font = "UbuntuMono:pixelsize=24:antialias=true:autohint=true:";
 //static char *font = "M+ 1mn:pixelsize=24:antialias=true:autohint=true:";
 static int borderpx = 20;
 
